30faffcef47d98711babffc69dce877189e35b83
Manar Monzer <mamonzer@2-C-6.42heilbronn.de>
2026-01-15 20:53:48 +0100

    finished two ex

---
+++ python_01/ex0/ft_garden_intro.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3.10
+if __name__ == '__main__':
+    plant = str("Rose")
+    height = int(25)
+    age = int(30)
+
+
+print("=== Welcome to My Garden ===")
+print(f"Plant: {plant}\n" f"Height: {height}cm\n" f"Age: {age} days\n")
+print("=== End of Program ===")
---
+++ python_01/ex1/ft_garden_data.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3.10
+
+class Plants:
+    #When we use def inside a class we call it method
+	def __init__(self, name, height, age):
+		self.name = name
+		self.height = str(height) + "cm"
+		self.age = str(age) + " days old"
+
+def main():
+	plant_1 = Plants('Rose', 25, 30)
+	plant_2 = Plants('Sunflower', 80, 45)
+	plant_3 = Plants('Cactus', 15, 120)
+
+	garden = [plant_1, plant_2, plant_3]
+
+	print("=== Garden Plant Registry ===")
+
+	for i in range(3):
+
+		plant = garden[i]
+		print(f"{plant.name}: "f"{plant.height}, " f"{plant.age} ")
+
+if __name__ == "__main__":
+    main()
